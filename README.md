8d0dc95c659dfb9a437ed04de3f80bf13bd766d5
Varun Ganapathi <varun@cloudlabs.io>
2016-02-26 18:44:03 -0800

    working version, only files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # goscrapetable
-Scrapes a table from a file or url and turns it into csv (or json)
+Scrapes a table from a html file and turns it into tab delimited file
+
+# how to use
+go get github.com/PuerkitoBio/goquery
+go get github.com/varung/goscrapetable
+
+run the binary passing it a html file
+
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
+		s.Find("td").Each(func(j int, s *goquery.Selection) {
+			fmt.Print(s.Text(), " ")
+		})
+		fmt.Println()
+	})
+}
